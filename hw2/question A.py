2eb891cda0b2a3e1f4b1273b2bb2383243a0f97d
ANGEL <angelxd84130@gmail.com>
2020-08-21 00:45:55 -0700

    Add files via upload

---
+++ hw2/question A.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import csv
+import statistics
+mpg = []
+cylinders = []
+displacement = []
+horsepower = []
+weight = []
+acceleration = []
+year = []
+count = 0
+
+with open('Auto.csv', newline='') as csvfile:
+    Auto = csv.reader(csvfile, quotechar='|')
+    for row in Auto:
+        if count > 0:
+            mpg.append(float(row[0]))
+            cylinders.append(int(row[1]))
+            displacement.append(float(row[2]))
+            if row[3]!='?':
+                horsepower.append(int(row[3])) #some ?
+            weight.append(int(row[4]))
+            acceleration.append((float(row[5])))
+            year.append(int(row[6]))
+        count += 1
+
+print("Range of mpg: ", min(mpg), '~', max(mpg), '/ mean: ',
+      sum(mpg)/count, '/ standard deviation: ', statistics.stdev(mpg))
+print("Range of cylinders: ", min(cylinders), '~', max(cylinders), '/ mean: ',
+      sum(cylinders)/count, '/ standard deviation: ', statistics.stdev(cylinders))
+print("Range of displacement: ", min(displacement), '~', max(displacement), '/ mean: ',
+      sum(displacement)/count, '/ standard deviation: ', statistics.stdev(displacement))
+print("Range of horsepower: ", min(horsepower), '~', max(horsepower), '/ mean: ',
+      sum(horsepower)/len(horsepower), '/ standard deviation: ', statistics.stdev(horsepower))
+print("Range of weight: ", min(weight), '~', max(weight), '/ mean: ',
+      sum(weight)/count, '/ standard deviation: ', statistics.stdev(weight))
+print("Range of acceleration: ", min(acceleration), '~', max(acceleration), '/ mean: ',
+      sum(acceleration)/count, '/ standard deviation: ', statistics.stdev(acceleration))
+print("Range of year: ", min(year), '~', max(year), '/ mean: ',
+      sum(year)/count, '/ standard deviation: ', statistics.stdev(year))
---
+++ hw2/question E.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import csv
+#from sklearn.model_selection import train_test_split
+#from sklearn.linear_model import LinearRegression
+
+mpg = []
+cylinders = []
+displacement = []
+horsepower = []
+weight = []
+acceleration = []
+year = []
+origin = []
+count = 0
+countHorse = 0
+with open('Auto.csv', newline='') as csvfile:
+    Auto = csv.reader(csvfile, quotechar='|')
+    for row in Auto:
+        if count > 0:
+            mpg.append(float(row[0]))
+            cylinders.append(int(row[1]))
+            displacement.append(float(row[2]))
+            if row[3]!='?':
+                horsepower.append(int(row[3])) #some ?
+                countHorse += 1
+            weight.append(int(row[4]))
+            acceleration.append((float(row[5])))
+            year.append(int(row[6]))
+            origin.append(int(row[7]))
+        count += 1
+
+plt.title("mpg v.s. displacement")
+plt.scatter(mpg, displacement, color="red")
+#linearRegressor = LinearRegression()
+#linearRegressor.fit(cylinders, weight)
+#plt.plot(cylinders, linearRegressor.predict(cylinders), color="blue")
+plt.xlabel("mpg")
+plt.ylabel("displacement")
+plt.show()
